b0ecaa5150316ba3630677c67e7eee16e7a1636f
Kazuma1107 <office.kz1227@gmail.com>
2022-03-10 10:26:59 +0000

    add PostImage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_165507) do
+ActiveRecord::Schema.define(version: 2022_03_10_102322) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "user_id"
+    t.text "caption"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
